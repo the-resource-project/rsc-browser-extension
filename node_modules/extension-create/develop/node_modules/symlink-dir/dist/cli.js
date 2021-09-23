ffe8ab3d3db68a951fd5c0f3df4945bb890a7cd6
Madeline OLeary <madeline.w.oleary@gmail.com>
2021-09-23 19:52:30 +0200

    fix: removing node modules

--- node_modules/extension-create/develop/node_modules/symlink-dir/dist/cli.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const linkDir = require(".");
-const args = process.argv.slice(2);
-linkDir(args[0], args[1]);
-//# sourceMappingURL=cli.js.map
