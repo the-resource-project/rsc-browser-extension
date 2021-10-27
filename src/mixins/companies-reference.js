e3fbc6714629dd0ea6566962205831d7d704f7cb
Madeline OLeary <madeline.w.oleary@gmail.com>
2021-10-27 18:08:13 +0200

    feat: adds a type of guide for the reference

--- src/mixins/companies-reference.js
+++ src/mixins/companies-reference.js
@@ -5,6 +5,13 @@
  export default {
    data () {
      return {
+       /**
+        * It should be noted that the subCategoryTypes pertain to categories, or the tab element present in each popup. These categories are defined in App.vue and used in the data () function.
+        */
+
+       /**
+        * These define the menu items under each tab
+        */
        subCategoryTypes: {
          "1": "Wages, Benefits, Contracts",
          "2": "Grievance Mechanism",
@@ -12,6 +19,9 @@
          "4": "Prevention of Underage & Forced Labor",
          "5": "Freedom of Association & Collective Bargaining"
        },
+       /**
+        * These define the points that pertain to each subcategory type
+        */
        pointTypes: {
          "1": {
            "1": "text 1",
@@ -39,6 +49,9 @@
            "3": "text 3"
          }
        },
+       /**
+        * A dictionary for the rating types
+        */
        ratingTypes: {
          "1": "good",
          "2": "neutral",
@@ -49,7 +62,6 @@
          "1": "RDR",
          "2": "KTC"
        }
-
      }
    }
  }
